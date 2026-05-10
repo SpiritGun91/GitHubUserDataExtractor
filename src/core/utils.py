87795c9f27148a39c0f253d15566b2efed17d34d
SpiritGun91 <spiritgun@vivaldi.net>
2026-05-09 23:24:41 -0400

    Fix D212 docstring summary placement in utils

--- src/core/utils.py
+++ src/core/utils.py
@@ -34,8 +34,7 @@ def get_auth_headers():
 
 
 def fetch_as_data_uri(url):
-    """
-    Fetch a remote image and return it as a base64 data URI.
+    """Fetch a remote image and return it as a base64 data URI.
 
     Embeds the image directly in the HTML file (needed when the page is served
     via file:// and WebKit would otherwise block external requests).
