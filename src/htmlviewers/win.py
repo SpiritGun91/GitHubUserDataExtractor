448d915edb70f2a7fa86e46e71a5a00a9bc40d3d
SpiritGun91 <spiritgun@vivaldi.net>
2026-05-09 23:47:35 -0400

    Fix Codacy pydocstyle: D212 summary placement and D203 blank line before class docstring

--- src/htmlviewers/win.py
+++ src/htmlviewers/win.py
@@ -7,6 +7,7 @@ from PyQt5.QtWebEngineWidgets import QWebEngineView
 
 
 class HTMLViewer(QMainWindow):
+
     """Main window that renders the generated HTML report."""
 
     def __init__(self, html_path):
